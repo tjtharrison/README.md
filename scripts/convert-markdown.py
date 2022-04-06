5898470d76e5ca7e6371faa96795a08e65b24799
tjtharrison <gh@tjth.co>
2022-04-06 17:24:18 +0100

    Added template to index pages

--- scripts/convert-markdown.py
+++ scripts/convert-markdown.py
@@ -45,19 +45,21 @@ for file_name in glob.iglob('**/**.md', recursive=True):
 for top_dir in glob.iglob('docs/pages/*'):
     # Write index file
     index_file = top_dir + "/index.html"
+    page_title = top_dir.split("/")[2]
     print("Collecting pages for " + index_file)
     page_list = []
 
     for indexable_page in glob.iglob(top_dir + "/*"):
         if "index.html" not in indexable_page:
-            nice_name = indexable_page.split("/")[-1]
-            page_list.append(nice_name)
+            link_name = indexable_page.split("/")[-1].replace(".html","")
+            page_list.append(link_name)
 
     # Build out index list
-    html = "<ul>"
+    html = "<ul>\n<h2>" + page_title + "</h2>"
     for page_file in page_list:
-        html = html + "<a href=" + page_file + ">" + page_file + "</a>\n"
-    html = html + "</ul>"
+        page_title = page_file.replace("_"," ").capitalize() 
+        html = html + "<li><a href=" + page_file + ">" + page_title + "</a></li>\n"
+    html = html + "</ul>\n"
 
     # Update template and save
     with open(template_file, 'r') as t:
